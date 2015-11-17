ca78f5f11287f2b9d026e124899a656e2ceb1bc3
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2015-11-17 10:34:56 +0100

    Upgrades biorob-cpp to verison 0.4.0~rc1

--- biorob-cpp.rb
+++ biorob-cpp.rb
@@ -3,22 +3,21 @@ require 'formula'
 class BiorobCpp < Formula
   homepage 'https://ponyo.epfl.ch/gitlab/core/projects/biorob-cpp'
   head 'https://ponyo.epfl.ch/gitlab/core/biorob-cpp.git' , :using => :git
-  url 'https://ponyo.epfl.ch/gitlab/core/biorob-cpp.git' , :using => :git, :tag => 'v0.3.2'
-  version '0.3.2'
+  url 'https://ponyo.epfl.ch/gitlab/core/biorob-cpp.git' , :using => :git, :tag => 'v0.4.0-rc1'
+  version '0.4.0~rc1'
 
   option :universal
 
   depends_on 'cmake' => :build
   depends_on 'biorob-cmake-utils' => :build
-  
+  depends_on 'pkgconfig' => :build
+  depends_on 'glog'
   def install
     ENV.universal_binary if build.universal?
     
     args = std_cmake_args
-    if MacOS.version == "10.9"
-      args << "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.8"
-      args << "-DCMAKE_BUIDL_TYPE=Release"
-    end
+    args << "-DCMAKE_BUIDL_TYPE=Release"
+
 
     system "cmake", ".", *args
     system "make install" 
